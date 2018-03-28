cd7964c8b32bd7af0ea6a7692657471a9d8bd188
Bartosz <bartosz.brzozowski23@gmail.com>
2018-03-28 19:22:13 +0200

    task 9:attributes

--- Circle.cpp
+++ Circle.cpp
@@ -6,11 +6,8 @@ Circle::Circle(double r) : r_(r) {}
 double Circle::getArea() const { return M_PI * r_ * r_; }
 double Circle::getPerimeter() const { return 2 * M_PI * r_; }
 double Circle::getRadius() const { return r_; }
-double Circle::getPi()
-{
-	return 3.14;
-}
-
+[[deprecated("Please use M_PI")]]
+double Circle::getPi() { return 3.14; }
 void Circle::print() const
 {
   std::cout << "Circle: radius: " << getRadius() << std::endl
--- Circle.hpp
+++ Circle.hpp
@@ -13,7 +13,8 @@ class Circle final : public Shape
   double getRadius() const;
   void print() const override;
   Circle() = delete;
-	double getPi();
+  double getPi();
+
  private:
   // doesn't allow to call default constructor
 
